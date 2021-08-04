802ba003e7a43ce0c00e0a137615616421d26345
zeynepmimutlu <zeynepranamutlu@outlook.com>
2021-08-05 00:49:38 +0300

    simple calculator UI

---
+++ calc.js
@@ -0,0 +1,18 @@
+function add(a,b){
+    return a+b
+}
+function subtract(a,b){
+    return a-b
+}
+function multiply(a,b){
+    return a*b
+}
+function divide(a,b){
+    return a/b
+}
+function operate(a,b,operator){
+    if (operator=='+') add(a,b)
+    else if (operator=='-') subtract(a,b)
+    else if (operator=='*') multiply(a,b)
+    else if(operator=='/') divide(a,b)
+}
