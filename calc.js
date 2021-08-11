8afbddd78b236790d4f7253642afdf2c1625ca7d
zeynepmimutlu <zeynepranamutlu@outlook.com>
2021-08-11 21:28:36 +0300

    Fixed bugs

--- calc.js
+++ calc.js
@@ -16,7 +16,10 @@ function operate(a,b,operator){
     if (operator=='+') return add(a,b)
     else if (operator=='-') return subtract(a,b)
     else if (operator=='*') return multiply(a,b)
-    else if(operator=='/') return divide(a,b)
+    else if(operator=='/'){
+      if (b==0) return 'YOU SHALL NOT PASS \nno kidding, you may pass but you shall not divide by zero'
+      else return divide(a,b)
+    }
 }
 
 function findButtonType(buttonClicked){
@@ -51,7 +54,7 @@ function calculator(){
                   a='0.'
                 }
                 else{
-                  a+='.'
+                  if (a.indexOf('.') < 0) a+='.'
                 }
               }
               else{
@@ -59,12 +62,12 @@ function calculator(){
                   b='0.'
                 }
                 else{
-                  b+='.'
+                  if (b.indexOf('.') < 0) b+='.'
                 }
               }
           }
           else if (typeOfButton== 'operator') {
-              if (a. split("").pop()=='.') a+='0'
+              if (a.split("").pop()=='.') a+='0'
               if (a=='') {    
                 a=''
                 b=''
@@ -83,7 +86,7 @@ function calculator(){
               } 
           }
           else if (typeOfButton== 'equalssign') {
-              if(a!='' && operator==''){
+              if(a!='' && b==''){
                 if (a.split("").pop()=='.') a+=0
                 result = a
                 a=result
@@ -105,7 +108,7 @@ function calculator(){
             operator=''
             result=''
           }       
-          displayScreen.innerText=a+operator+b+result     
+          displayScreen.innerText=a+operator+b+result
   })
 }
 }
